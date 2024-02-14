1769fb1f0d57f8366c8bceb1cc13514246176c55
MinJoon Joo <tuk34896@temple.edu>
2024-02-14 15:40:39 -0500

    update on main
    import linearlayoutmanager and bring it, also adapter need to generate set of numbers 1-100

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -3,6 +3,7 @@ package edu.temple.simplerecyclerview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.LinearLayoutManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,8 +14,9 @@ class MainActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
 
         //Step 2: Provide a LayoutManager
-
-
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val numbers = Array<Int>(100){i -> (i+1)}
         //Step 4: Provide a RecyclerView.Adapter
+        recyclerView.adapter = NumberDisplayAdapter(numbers)
     }
 }

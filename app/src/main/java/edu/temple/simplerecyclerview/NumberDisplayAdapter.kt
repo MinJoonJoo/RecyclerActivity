7b5936e095e48756db9b82413e3852625ceeef13
MinJoon Joo <tuk34896@temple.edu>
2024-02-14 15:41:18 -0500

    Update numberdisplayadapter
    imported viewgroup and provided adapterparent, override functions needed

--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -2,13 +2,24 @@ package edu.temple.simplerecyclerview
 
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-
+import android.view.ViewGroup
 // Step 3: Complete adapter implementation
 
-class NumberDisplayAdapter /* Step 3a: Provide Adapter Parent */ {
+class NumberDisplayAdapter(private val arrayInt: Array<Int>): RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>()
+/* Step 3a: Provide Adapter Parent */ {
 
     class NumberViewHolder (val textView: TextView) : RecyclerView.ViewHolder (textView) {}
 
     //Step 3b: Complete function definitions for adapter
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
+        TODO("Not yet implemented")
+    }
 
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: NumberViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
